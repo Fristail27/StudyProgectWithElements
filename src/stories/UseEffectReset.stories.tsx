e16de3d776e64b2dbd31fb2e86e2b569d57e6bb9
Fristail27 <fristail27@gmail.com>
2021-01-31 16:01:49 +0300

    useeffect cleanup

---
+++ src/stories/UseEffectReset.stories.tsx
@@ -0,0 +1,47 @@
+import React, {useEffect, useState} from "react";
+
+export default {
+    title: "useEffect demo",
+}
+
+export const ResetEffectExample = () => {
+    const [counter, setCounter] = useState(1)
+
+    console.log("component rendered")
+
+    useEffect(()=>{
+        console.log("Effect occurred" + counter)
+        return ()=> {
+            console.log("reset effect" + counter)}
+    },[counter])
+
+    return (
+        <>
+            hello, {counter}
+            <button onClick={()=>{setCounter(counter+1)}}>x</button>
+        </>
+    )
+};
+
+export const KeysTrackerExample = () => {
+    const [text, setText] = useState("")
+
+    console.log("component rendered with " + text)
+
+    useEffect(()=>{
+        const handler = (e:KeyboardEvent)=>{
+            console.log(e.code)
+            setText(text + e.key)
+        }
+        window.document.addEventListener("keypress", handler)
+        return ()=> {
+            window.removeEventListener("keypress", handler)
+        }
+    },[text])
+
+    return (
+        <>
+            typed text: {text}
+        </>
+    )
+};
